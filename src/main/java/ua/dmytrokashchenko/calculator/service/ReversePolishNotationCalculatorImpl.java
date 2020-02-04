db41011ee96cededa64e6e4dafaf55a164000da4
Dmytro Kashchenko1 <dmytro_kashchenko1@epam.com>
2020-02-04 15:22:41 +0200

    add logging

--- src/main/java/ua/dmytrokashchenko/calculator/service/ReversePolishNotationCalculatorImpl.java
+++ src/main/java/ua/dmytrokashchenko/calculator/service/ReversePolishNotationCalculatorImpl.java
@@ -1,5 +1,7 @@
 package ua.dmytrokashchenko.calculator.service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import ua.dmytrokashchenko.calculator.domain.Element;
 
 import java.util.HashMap;
@@ -9,6 +11,9 @@ import java.util.function.BinaryOperator;
 
 public class ReversePolishNotationCalculatorImpl implements Calculator {
     private static final Map<Element, BinaryOperator<Double>> OPERATOR_TO_FUNCTION = new HashMap<>();
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(ReversePolishNotationCalculatorImpl.class);
+
     private final ReversePolishNotationParser parser;
 
     static {
@@ -26,6 +31,13 @@ public class ReversePolishNotationCalculatorImpl implements Calculator {
     @Override
     public double calculate(String expression) {
         List<Element> elements = parser.parse(expression);
+
+        boolean isDifficult = elements.size() > 3;
+        if (isDifficult) {
+            LOGGER.debug("The expression for calculation: " + expression);
+            LOGGER.debug("Reverse Polish Notation: " + elements);
+        }
+
         while (elements.size() > 2) {
             for (int i = 2; i < elements.size(); i++) {
                 if (elements.get(i).isOperand()) {
@@ -37,7 +49,14 @@ public class ReversePolishNotationCalculatorImpl implements Calculator {
                 }
             }
         }
-        return Double.parseDouble(elements.get(0).getValue());
+
+        double result = Double.parseDouble(elements.get(0).getValue());
+
+        if (isDifficult) {
+            LOGGER.debug("Result: " + result);
+        }
+
+        return result;
     }
 
     private double calculateElements(Element firstOperand, Element secondOperand, Element operator) {
